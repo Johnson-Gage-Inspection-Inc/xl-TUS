d9cc6d336e210fc7d56c5bf0bb565d3ae528fff6
Jeff Hall <rhythmatician5@gmail.com>
2025-09-24 16:43:17 -0500

    Refactor WireSets data retrieval and update Main for traceability numbers and retirement date calculations

--- exploded/New TUS (Beta)/queries/WireSets.m
+++ exploded/New TUS (Beta)/queries/WireSets.m
@@ -5,8 +5,7 @@ let
             Source = Excel.Workbook(Web.Contents("https://jgiquality.sharepoint.com/sites/JGI/Shared Documents/Pyro/WireSetCerts.xlsx"), null, true),
             Sheet1_Sheet = Source{[Item="Sheet1",Kind="Sheet"]}[Data],
             #"Promoted Headers" = Table.PromoteHeaders(Sheet1_Sheet, [PromoteAllScalars=true]),
-            #"Removed Other Columns" = Table.SelectColumns(#"Promoted Headers",{"type", "wire_roll_cert_number", "certificate_number", "next_service_date", "service_date", "custom_order_number", "asset_tag", "serial_number", "asset_id"}),
-            #"Changed Type" = Table.TransformColumnTypes(#"Removed Other Columns",{{"next_service_date", type date}, {"service_date", type date}, {"asset_id", Int64.Type}})
+            #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"service_date", type date}, {"asset_id", Int64.Type}})
         in
             #"Changed Type"
     otherwise null,
@@ -15,7 +14,7 @@ let
     CachedData = try
         let
             existing = Excel.CurrentWorkbook(){[Name="WireSets"]}[Content],
-            #"Changed Type" = Table.TransformColumnTypes(existing,{{"next_service_date", type date}, {"service_date", type date}})
+            #"Changed Type" = Table.TransformColumnTypes(existing,{{"service_date", type date}})
         in
             #"Changed Type"
         otherwise null,
@@ -24,7 +23,7 @@ let
     FinalData = if FreshData <> null then FreshData 
                 else if CachedData <> null then CachedData
                 else #table(
-                    {"type", "wire_roll_cert_number", "certificate_number", "next_service_date", "service_date", "custom_order_number", "asset_tag", "serial_number", "asset_id"},
+                    {"asset_id",  "serial_number", "asset_tag", "custom_order_number", "service_date", "certificate_number", "wire_roll_cert_number", "type"},
                     {{"No data available", "", "", null, null, "", "", "", 0}}
                 )
 in
