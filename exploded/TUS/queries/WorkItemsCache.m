2adffdc1c88a3e2926dc731bcb8f997af1e08209
Jeff Hall <rhythmatician5@gmail.com>
2026-04-02 16:11:39 -0500

    Harden WorkItemsCache to use CacheColumns for EmptyCache and improve data validation handling

--- exploded/TUS/queries/WorkItemsCache.m
+++ exploded/TUS/queries/WorkItemsCache.m
@@ -8,7 +8,9 @@
 let
     WorkOrderNumber = try Text.From(Excel.CurrentWorkbook(){[Name="WorkOrderNumber"]}[Content][Column1]{0}) otherwise "",
 
-    EmptyCache = #table({"AsFoundCheck", "AsFoundResult", "AsLeftCheck", "AsLeftResult", "AssetCompanyId", "AssetCompanyName", "AssetDescription", "AssetId", "AssetName", "AssetOwnership", "AssetSiteId", "AssetSiteName", "AssetTag", "CertificateNumber", "ChannelCount", "CheckedById", "CheckedByName", "CheckedOn", "ClientCompanyId", "CompletedById", "CompletedByName", "CompletedOn", "CreatedOnUtc", "CustomOrderNumber", "DocumentNumber", "DocumentSection", "DueDate", "IsLimited", "ItemAsFoundResult", "ItemAsLeftResult", "ItemResultStatus", "MaintenancePlan", "MaintenanceTask", "NextServiceDate", "NextServiceLevel", "OrderItemNumber", "OverridePartsTotal", "OverrideRepairsTotal", "OverrideServiceTotal", "PartsTotal", "PartsTotalBeforeDiscount", "ProviderCompany", "ProviderPhone", "ProviderTechnician", "RepairsTotal", "ResultStatus", "SerialNumber", "ServiceCharge", "ServiceComments", "ServiceDate", "ServiceLevel", "ServiceLevelDocumentNumber", "ServiceNotes", "ServiceOptions", "ServiceOrderId", "ServiceTotal", "ServiceType", "UpdatedBy", "UpdatedById", "UpdatedOnUtc", "VendorCompanyId", "WorkItemId", "WorkStatus"}, {}),
+    CacheColumns = {"AsFoundCheck", "AsFoundResult", "AsLeftCheck", "AsLeftResult", "AssetCompanyId", "AssetCompanyName", "AssetDescription", "AssetId", "AssetName", "AssetOwnership", "AssetSiteId", "AssetSiteName", "AssetTag", "CertificateNumber", "ChannelCount", "CheckedById", "CheckedByName", "CheckedOn", "ClientCompanyId", "CompletedById", "CompletedByName", "CompletedOn", "CreatedOnUtc", "CustomOrderNumber", "CustomWorkStatus", "DocumentNumber", "DocumentSection", "DueDate", "IsLimited", "ItemAsFoundResult", "ItemAsLeftResult", "ItemResultStatus", "MaintenancePlan", "MaintenanceTask", "NextServiceDate", "NextServiceLevel", "OrderItemNumber", "OverridePartsTotal", "OverrideRepairsTotal", "OverrideServiceTotal", "PartsTotal", "PartsTotalBeforeDiscount", "ProviderCompany", "ProviderPhone", "ProviderTechnician", "RepairsTotal", "ResultStatus", "SerialNumber", "ServiceCharge", "ServiceComments", "ServiceDate", "ServiceLevel", "ServiceLevelDocumentNumber", "ServiceNotes", "ServiceOptions", "ServiceOrderId", "ServiceTotal", "ServiceType", "UpdatedBy", "UpdatedById", "UpdatedOnUtc", "VendorCompanyId", "WorkItemId", "WorkStatus"},
+
+    EmptyCache = #table(CacheColumns, {}),
 
     FreshData =
         if Text.Length(WorkOrderNumber) = 0 then
@@ -27,7 +29,7 @@ let
                     )
                 in
                     if Value.Is(Source, type list) then
-                        try Table.FromRecords(Source) otherwise null
+                        try Table.FromRecords(Source, CacheColumns, MissingField.UseNull) otherwise null
                     else
                         null
             otherwise
@@ -37,7 +39,7 @@ let
         try
             let
                 cached = Excel.CurrentWorkbook(){[Name="WorkItemsCache"]}[Content],
-                validated = if Table.RowCount(cached) > 0 then cached else null
+                validated = if Table.RowCount(cached) > 0 then Table.SelectColumns(cached, CacheColumns, MissingField.UseNull) else null
             in
                 validated
         otherwise
