b6c4b08baab950ca092b8525bf6df5f12ab2557c
Jeff Hall <rhythmatician5@gmail.com>
2026-04-03 17:01:02 -0500

    Stop trying to correct capitalization in customers query

--- exploded/TUS/queries/customers.m
+++ exploded/TUS/queries/customers.m
@@ -17,8 +17,7 @@ let
             #"Expanded shipping_address" = Table.ExpandRecordColumn(#"Removed Other Columns", "shipping_address", {"additional_properties"}, {"additional_properties"}),
             #"Expanded additional_properties" = Table.ExpandRecordColumn(#"Expanded shipping_address", "additional_properties", {"Address1", "Address2", "Attention1", "Attention2", "City", "Company", "Country", "Email", "FaxNumber", "FirstName", "LastName", "PhoneNumber", "StateProvince", "StateProvinceAbbreviation", "ZipPostalCode"}, {"Address1", "Address2", "Attention1", "Attention2", "City", "Company", "Country", "Email", "FaxNumber", "FirstName", "LastName", "PhoneNumber", "StateProvince", "StateProvinceAbbreviation", "ZipPostalCode"}),
             #"Reordered Columns" = Table.ReorderColumns(#"Expanded additional_properties",{"company_name", "Address1", "Address2", "City", "StateProvinceAbbreviation", "ZipPostalCode", "company_id"}),
-            #"Capitalized Each Word" = Table.TransformColumns(#"Reordered Columns",{{"City", Text.Proper, type text}, {"Address1", Text.Proper, type text}, {"Address2", Text.Proper, type text}}),
-            #"Replaced Value1" = Table.ReplaceValue(#"Capitalized Each Word",null,"KS",Replacer.ReplaceValue,{"StateProvinceAbbreviation"}),
+            #"Replaced Value1" = Table.ReplaceValue(#"Reordered Columns",null,"KS",Replacer.ReplaceValue,{"StateProvinceAbbreviation"}),
             #"Merged Columns" = Table.CombineColumns(#"Replaced Value1",{"City", "StateProvinceAbbreviation", "ZipPostalCode"},Combiner.CombineTextByDelimiter(", ", QuoteStyle.None),"Merged"),
             #"Replaced Value" = Table.ReplaceValue(#"Merged Columns",null,"",Replacer.ReplaceValue,{"Address2"}),
             #"Sorted Rows" = Table.Sort(#"Replaced Value",{{"company_name", Order.Ascending}})
